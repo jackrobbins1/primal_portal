7f21bc47d4b2ef2f64701f99b2bf953a1b0f4f2a
Jackson Robbins <jackrobbins1@gmail.com>
2019-07-25 12:00:48 -0500

    Updates

---
+++ precache-manifest.ba923373bb9f97b257dc279154e30ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027ff3346888c83e6a3a30f8d3363bd6",
+    "url": "/primal_portal/index.html"
+  },
+  {
+    "revision": "a6504e80645f6f18ba9c",
+    "url": "/primal_portal/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "396aaa3218b50821ebb6",
+    "url": "/primal_portal/static/css/main.0c5de245.chunk.css"
+  },
+  {
+    "revision": "a6504e80645f6f18ba9c",
+    "url": "/primal_portal/static/js/2.f9ba2f41.chunk.js"
+  },
+  {
+    "revision": "396aaa3218b50821ebb6",
+    "url": "/primal_portal/static/js/main.4a1bbb72.chunk.js"
+  },
+  {
+    "revision": "76265ee9ae46c93e301f",
+    "url": "/primal_portal/static/js/runtime~main.e81b8bdf.js"
+  }
+]);
